a0f16ba1bd5ddad8a634fbf0cf06bece463c0571
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2021-12-13 11:50:26 +0100

    Rename directory; solve day three part one

---
+++ lib/days/03_day_three/day_three.ex
@@ -0,0 +1,63 @@
+defmodule Aoc21.DayThree do
+  use Aoc21.Elves
+
+  defp parse_line(line, acc \\ [])
+  defp parse_line(<<>>, acc), do: acc
+
+  defp parse_line(<<h::binary-size(1), rest::binary>>, acc) do
+    h = to_int(h)
+    parse_line(rest, acc ++ [h])
+  end
+
+  # expected to return 2648450
+  # We're using Nx tensors to get the mean over the Y axis
+  # By rounding the mean we're transforming to 1.0 / 0.0 values.
+  # Feeding the flattened list to trunc/1 gives us a list of integer values
+  # turning these integer values into an bitstring and grabbing the numerical value
+  # from that gives us the gamma. XOR-ing with a mask of ones gives us the epsilon.
+  # multiplying those together gives us the answer of part A
+  def part_one do
+    averages =
+      input_as_lines()
+      |> Enum.map(&parse_line/1)
+      |> Nx.tensor()
+      |> Nx.mean(axes: [0])
+      |> Nx.round()
+      |> Nx.to_flat_list()
+      |> Enum.map(&trunc/1)
+
+    bit_length = length(averages)
+
+    gamma =
+      averages
+      |> list_to_binary()
+      |> grab_number()
+
+    mask =
+      bit_length
+      |> create_mask()
+      |> grab_number()
+
+    epsilon = Bitwise.bxor(gamma, mask)
+
+    gamma * epsilon
+  end
+
+  defp list_to_binary(list) do
+    for n <- list, reduce: <<>> do
+      acc -> <<acc::bitstring, n::size(1)>>
+    end
+  end
+
+  defp create_mask(length) do
+    for _ <- 1..length, reduce: <<>> do
+      acc -> <<acc::bitstring, 1::size(1)>>
+    end
+  end
+
+  defp grab_number(binary) do
+    bit_length = bit_size(binary)
+    <<number::size(bit_length)>> = binary
+    number
+  end
+end
